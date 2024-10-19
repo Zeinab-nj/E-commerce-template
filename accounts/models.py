6da5d7e3787fb358ac986e0e00e8aab2444984a3
zeinab-nj <zeinab.najafi79@yahoo.com>
2024-10-19 20:49:42 +0330

    usermanager added

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from .managers import managers
 
-# Create your models here.
+class User(AbstractBaseUser):
+    email = models.EmailField(max_length=250, unique=True)
+    phone_number = models.CharField(max_length=11, unique=True)
+    full_name = models.CharField(max_length=250)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'phone_number'  # Authenticate in login
+    REQUIRED_FIELDS = ['email']  # just it uses in createsuperuser
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
