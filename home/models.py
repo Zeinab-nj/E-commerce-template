62db76d8d151d8c19ba43cd648a350a014253b85
zeinab-nj <zeinab.najafi79@yahoo.com>
2024-11-04 13:33:36 +0330

    category & product models added

--- home/models.py
+++ home/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=100, unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'category'
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='product')
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=150, unique=True)
+    price = models.IntegerField()
+    image = models.ImageField(upload_to='products/%Y/%m/%d/', null=True, blank=True)
+    description = models.TextField(max_length=1000)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
